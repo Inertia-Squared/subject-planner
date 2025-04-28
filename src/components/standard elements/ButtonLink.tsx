d8330efa9dc753ecc2b55d6afc809d48a2a4f767
Inertia <ojmiddlet@gmail.com>
2025-04-29 00:15:59 +1000

    - Minor Aesthetic changes

--- src/components/standard elements/ButtonLink.tsx
+++ src/components/standard elements/ButtonLink.tsx
@@ -8,7 +8,7 @@ interface ButtonLinkProps extends LinkProps {
 
 export const ButtonLink = (props: ButtonLinkProps) => {
     return (
-        <Link className={`p-1.5 rounded border-2 border-amber-500 bg-amber-200 font-semibold drop-shadow-md hover:bg-amber-300 hover:border-amber-600 hover:drop-shadow-none ease-out hover:translate-y-[1px] duration-300 ${props.className}`} {...props}>{props.children}</Link>
+        <Link className={`p-1.5 rounded border-2 border-amber-500 bg-amber-200 text-lg font-semibold drop-shadow-md hover:bg-amber-300 hover:border-amber-600 hover:drop-shadow-none ease-out hover:translate-y-[1px] duration-300 ${props.className}`} {...props}>{props.children}</Link>
     )
 
 }
